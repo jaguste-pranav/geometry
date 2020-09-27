34358fb318fd00175423c0348977def045c5f642
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-09-27 21:44:30 +0530

    [pranav] Add. UC2

--- geometry/Program.cs
+++ geometry/Program.cs
@@ -6,24 +6,47 @@ namespace geometry
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the two points:");
+            int x1, x2, y1, y2;
+            Console.WriteLine("Enter the two points for 1st Line:");
 
             Console.WriteLine("Enter x1 coordinate: ");
-            int x1 = Convert.ToInt32(Console.ReadLine());
+            x1 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Enter y1 coordinate: ");
-            int y1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Enter x2 coordinate: ");
-            int x2 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Enter y2 coordinate: ");
-            int y2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
 
-            double distance = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+            double distanceForLine1 = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
 
-            Console.WriteLine("The distnace between these points is " + distance);
+            Console.WriteLine("Enter the two points for 2nd Line:");
 
+            Console.WriteLine("Enter x1 coordinate: ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter y1 coordinate: ");
+            y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter x2 coordinate: ");
+            x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter y2 coordinate: ");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            double distanceForLine2 = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+
+            if (distanceForLine1 == distanceForLine2)
+            {
+                Console.WriteLine("The lines are of equal distance");
+            }
+            else
+            {
+                Console.WriteLine("The lines are not of equal distance");
+            }
         }
     }
 }
