a03c43dbed47bf217a9b160d8ff45824270ff1c6
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-09-29 23:27:23 +0530

    [pranav] Add. Class implementation

---
+++ geometry/Points.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace geometry
+{
+    class Points
+    {
+        private int x, y;
+        public void setXCoordinate(int x)
+        {
+            this.x = x;
+        }
+
+        public void setYCoordinate(int y)
+        {
+            this.y = y;
+        }
+
+        public int getXCoordinate()
+        {
+            return this.x;
+        }
+
+        public int getYCoordinate()
+        {
+            return this.y;
+        }
+
+        public static double distance(int x1, int y1, int x2, int y2)
+        {
+            double distance = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+            Console.WriteLine("The distance between two points is "+ distance);
+
+            return distance;
+        }
+    }
+}
--- geometry/Program.cs
+++ geometry/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace geometry
 {
@@ -6,53 +7,87 @@ namespace geometry
     {
         static void Main(string[] args)
         {
-            int x1, x2, y1, y2;
-            Console.WriteLine("Enter the two points for 1st Line:");
+            Points point_1 = new Points();
+            Points point_2 = new Points();
 
-            Console.WriteLine("Enter x1 coordinate: ");
-            x1 = Convert.ToInt32(Console.ReadLine());
+            point_1.setXCoordinate(6);
+            point_1.setYCoordinate(8);
 
-            Console.WriteLine("Enter y1 coordinate: ");
-            y1 = Convert.ToInt32(Console.ReadLine());
+            point_2.setXCoordinate(0);
+            point_2.setYCoordinate(0);
 
-            Console.WriteLine("Enter x2 coordinate: ");
-            x2 = Convert.ToInt32(Console.ReadLine());
+            Points point_3 = new Points();
+            Points point_4 = new Points();
 
-            Console.WriteLine("Enter y2 coordinate: ");
-            y2 = Convert.ToInt32(Console.ReadLine());
+            point_3.setXCoordinate(6);
+            point_3.setYCoordinate(7);
 
-            double distanceForLine1 = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+            point_4.setXCoordinate(0);
+            point_4.setYCoordinate(0);
 
-            Console.WriteLine("Enter the two points for 2nd Line:");
+            double distForline1 = Points.distance(point_1.getXCoordinate(), point_1.getYCoordinate(), point_2.getXCoordinate(), point_2.getYCoordinate());
+            double distForline2 = Points.distance(point_3.getXCoordinate(), point_3.getYCoordinate(), point_4.getXCoordinate(), point_4.getYCoordinate());
 
-            Console.WriteLine("Enter x1 coordinate: ");
-            x1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter y1 coordinate: ");
-            y1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter x2 coordinate: ");
-            x2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter y2 coordinate: ");
-            y2 = Convert.ToInt32(Console.ReadLine());
-
-            double distanceForLine2 = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
-
-            if (distanceForLine1 == distanceForLine2)
+            if(distForline1 == distForline2)
             {
                 Console.WriteLine("The lines are of equal distance");
             }
 
-            if (distanceForLine1 > distanceForLine2)
+            if(distForline1>distForline2)
             {
-                Console.WriteLine("The line 1 is larger than line 2");
+                Console.WriteLine("Line 1 is greater in length");
             }
 
-            if (distanceForLine1 < distanceForLine2)
+            if (distForline2 > distForline1)
             {
-                Console.WriteLine("The line 2 is larger than line 1");
+                Console.WriteLine("Line 2 is greater in length");
             }
+            //    Console.WriteLine("Enter the two points for 1st Line:");
+
+            //    Console.WriteLine("Enter x1 coordinate: ");
+            //    x1 = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("Enter y1 coordinate: ");
+            //    y1 = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("Enter x2 coordinate: ");
+            //    x2 = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("Enter y2 coordinate: ");
+            //    y2 = Convert.ToInt32(Console.ReadLine());
+
+            //    double distanceForLine1 = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+
+            //    Console.WriteLine("Enter the two points for 2nd Line:");
+
+            //    Console.WriteLine("Enter x1 coordinate: ");
+            //    x1 = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("Enter y1 coordinate: ");
+            //    y1 = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("Enter x2 coordinate: ");
+            //    x2 = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("Enter y2 coordinate: ");
+            //    y2 = Convert.ToInt32(Console.ReadLine());
+
+            //    double distanceForLine2 = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+
+            //    if (distanceForLine1 == distanceForLine2)
+            //    {
+            //        Console.WriteLine("The lines are of equal distance");
+            //    }
+
+            //    if (distanceForLine1 > distanceForLine2)
+            //    {
+            //        Console.WriteLine("The line 1 is larger than line 2");
+            //    }
+
+            //    if (distanceForLine1 < distanceForLine2)
+            //    {
+            //        Console.WriteLine("The line 2 is larger than line 1");
+            //    }
         }
     }
 }
